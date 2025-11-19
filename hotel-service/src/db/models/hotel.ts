f15e8667d5b309c73d79e9eda0a84d2034f5ba11
dibyajyoti79 <dibyajyoti274@gmail.com>
2025-11-20 01:31:15 +0530

    feat: Implement Hotel model and establish database connection on server start

---
+++ hotel-service/src/db/models/hotel.ts
@@ -0,0 +1,70 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from "sequelize";
+import sequelize from "./sequelize";
+
+class Hotel extends Model<
+  InferAttributes<Hotel>,
+  InferCreationAttributes<Hotel>
+> {
+  declare id: CreationOptional<number>;
+  declare name: string;
+  declare address: string;
+  declare location: string;
+  declare rating: number;
+  declare rating_count: number;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+}
+
+Hotel.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.DECIMAL(3, 2),
+      allowNull: true,
+    },
+    rating_count: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    tableName: "hotels",
+    underscored: true,
+    timestamps: true,
+  }
+);
+
+export default Hotel;
--- hotel-service/src/server.ts
+++ hotel-service/src/server.ts
@@ -4,6 +4,8 @@ import v1Router from "./routes/v1/index.routes";
 import { appErrorHandler } from "./middlewares/error.middleware";
 import logger from "./config/logger.config";
 import { attachCorrelationIdMiddleware } from "./middlewares/correlation.middleware";
+import sequelize from "./db/models/sequelize";
+import Hotel from "./db/models/hotel";
 const app = express();
 
 app.use(express.json());
@@ -13,7 +15,13 @@ app.use("/api/v1", v1Router);
 
 app.use(appErrorHandler);
 
-app.listen(serverConfig.PORT, () => {
+app.listen(serverConfig.PORT, async () => {
   logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
   logger.info(`Press Ctrl+C to stop the server.`);
+  try {
+    await sequelize.authenticate();
+    logger.info("Database connected successfully");
+  } catch (error) {
+    logger.error("Database connection failed", error);
+  }
 });
